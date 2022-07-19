9a5940334c251accc2f9523f42a609714fe113c1
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-19 16:19:12 -0400

    chore: built package

--- build/lib/cachier_common_library/DriverType.py
+++ build/lib/cachier_common_library/DriverType.py
@@ -3,3 +3,11 @@ class DriverType:
     JSON = 'json'
     SQLITE = 'sqlite'
     PICKLE = 'pickle'
+
+    def is_valid(self: 'DriverType', driver_type: str) -> bool:
+        return driver_type in [
+            self.RAM,
+            self.JSON,
+            self.SQLITE,
+            self.PICKLE,
+        ]
