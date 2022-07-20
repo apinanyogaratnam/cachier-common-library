4cdbe584c4be0ced1577f031cfae3bbb596743c5
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-20 12:00:51 -0400

    feat: created validate_version function to validate versions

--- update_version.py
+++ update_version.py
@@ -15,3 +15,11 @@ with open('setup.py', 'r') as file:
             break
 
 print('setup.py version', version)
+
+
+def validate_version(version) -> bool:
+    if len(version.split('.')) != 3:
+        raise ValueError('Version must be in the format x.y.z')
+    for part in version.split('.'):
+        if not part.isdigit():
+            raise ValueError('Version must be in the format x.y.z')
