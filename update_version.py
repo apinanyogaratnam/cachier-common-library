38c069f61a716a12c7054547f57ab5c8dfb4f0ad
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-20 11:58:22 -0400

    feat: printing current version of makefile

---
+++ update_version.py
@@ -0,0 +1,8 @@
+with open('Makefile', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        if 'VERSION' in line:
+            version = line.split('=')[1].strip()
+            break
+
+print('version', version)
