4de494a11afa3f302f8dc8dd38469e73de94389f
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-20 11:59:50 -0400

    feat: printing setup.py version

--- update_version.py
+++ update_version.py
@@ -6,3 +6,12 @@ with open('Makefile', 'r') as file:
             break
 
 print('makefile version', version)
+
+with open('setup.py', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        if 'version' in line:
+            version = line.split('=')[1].strip().strip(',').strip("'")
+            break
+
+print('setup.py version', version)
