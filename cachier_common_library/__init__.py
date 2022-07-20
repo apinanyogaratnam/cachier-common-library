788a6e5cffc287245a37773d66ea30a3a76bf182
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-19 20:37:54 -0400

    refactor: exposed DriverType class using __all__

--- cachier_common_library/__init__.py
+++ cachier_common_library/__init__.py
@@ -2,9 +2,6 @@ from cachier_common_library.DriverType import DriverType
 from cachier_common_library.DriverTypeError import DriverTypeError
 
 __all__ = [
+    'DriverType',
     'DriverTypeError',
 ]
-
-
-class DriverType(DriverType):
-    pass
