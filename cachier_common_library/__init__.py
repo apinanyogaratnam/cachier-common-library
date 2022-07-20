dc7dd2f122b6176825bfa19cce73b39f4f829f63
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-19 20:37:31 -0400

    feat: exposed DriverTypeError class in cachier_common_library package

--- cachier_common_library/__init__.py
+++ cachier_common_library/__init__.py
@@ -1,4 +1,9 @@
 from cachier_common_library.DriverType import DriverType
+from cachier_common_library.DriverTypeError import DriverTypeError
+
+__all__ = [
+    'DriverTypeError',
+]
 
 
 class DriverType(DriverType):
